7239315c2a865e25357a18f20d370cf1c46d31f2
Henrique Serigati <serigatihenrique@gmail.com>
2023-08-10 20:54:51 -0300

    terceiro commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function desvia(btn) {
     btn.style.position = 'absolute';
     btn.style.bottom = geraPosicao(10, 90);
     btn.style.left = geraPosicao(10, 90);
-}
+}   
 
 function geraPosicao(min, max) {
     return (Math.random() * (max - min) + min) + "%";
